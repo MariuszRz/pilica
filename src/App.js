84086ee051b39c60181c57ab5d51b9fc6d789b0f
MariuszRz <45454410+MariuszRz@users.noreply.github.com>
2020-09-13 22:07:57 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyled from 'utils/Globalsyle';
 import theme from 'utils/theme';
-import { Section } from './app.css';
+import { Section, Aside, Main } from './app.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Header } from 'components';
 import { HomePage, News, History } from 'pages';
@@ -13,13 +13,47 @@ function App() {
         <GlobalStyled />
         <Header />
         <Section>
-          <Switch>
-            <Route path={`/`} exact component={HomePage} />
-            <Route path={`/news`} exact component={News} />
-            <Route path={`/history`} exact component={History} />
-            <Route path={`/login`} component={HomePage} />
-            <Route component={HomePage} />
-          </Switch>
+          <Aside>
+            <nav>
+              <h2>Aktualności</h2>
+              <ul>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+              </ul>
+            </nav>
+            <nav>
+              <h2>Historia</h2>
+              <ul>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+                <li>Link</li>
+              </ul>
+            </nav>
+          </Aside>
+          <Main>
+            <Switch>
+              <Route path={`/`} exact component={HomePage} />
+              <Route path={`/news`} exact component={News} />
+              <Route path={`/history`} exact component={History} />
+              <Route path={`/login`} component={HomePage} />
+              <Route component={HomePage} />
+            </Switch>
+          </Main>
         </Section>
       </ThemeProvider>
     </BrowserRouter>
--- src/app.css.js
+++ src/app.css.js
@@ -4,5 +4,54 @@ export const Section = styled.section`
   max-width: 1024px;
   min-height: 90vh;
   margin: 0 auto;
-  box-shadow: 0 3px 6px ${({ theme }) => theme.shadow};
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Aside = styled.aside`
+  min-width: 224px;
+  padding: 5px 0;
+
+  nav {
+    padding: 5px 10px;
+    margin: 15px 0;
+    border-radius: 15px;
+    box-shadow: 5px 5px 10px ${({ theme }) => theme.shadow};
+
+    h2 {
+      margin: 0 5px;
+    }
+
+    ul {
+      margin: 5px;
+      padding: 0px;
+
+      li {
+        list-style-type: none;
+        padding-left: 5px;
+
+        &::before {
+          content: '';
+          padding-right: 5px;
+          opacity: 0;
+          transition: opacity 0.1s ease-in-out;
+          border-left: 2px solid ${({ theme }) => theme.color.blue.light};
+        }
+
+        &:hover {
+          color: ${({ theme }) => theme.color.blue.light};
+          cursor: pointer;
+        }
+
+        &:hover::before {
+          opacity: 1;
+        }
+      }
+    }
+  }
+`;
+
+export const Main = styled.main`
+  min-width: 800px;
+  padding: 20px 10px;
 `;
